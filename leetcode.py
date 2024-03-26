92678f5e392ea2fcabce8e023b9d665dc142a9e3
胡金栋 <jameshopbourn@gmail.com>
2024-03-26 10:20:17 +0800

    update leetcode.py code
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

--- leetcode.py
+++ leetcode.py
@@ -3,20 +3,14 @@ import re
 import sys
 import json
 
-def camel_to_snake(camel_case_string):
-    snake_case_string = re.sub(r'([A-Z])', r'-\1', camel_case_string)
-    return snake_case_string.lstrip('-').lower()
-
 data = open('result.json').read()
 data = json.loads(data)
 result = []
 for item in data:
+    if sys.argv[1] not in item["titleCN"] and sys.argv[1].lower() not in item["titleUS"].lower():
+        continue
     item["icon"] = {}
     item["icon"]["path"] =  "icon.png"
-    if sys.argv[1].isalpha() and camel_to_snake(sys.argv[1]) in item["arg"]:
-        pass
-    elif sys.argv[1].lower() not in item["title"].lower():
-        continue
     if os.environ['alfred_workflow_keyword'].lower() == "lc":
         item["arg"] = f"https://leetcode.cn/problems/{item['arg']}/description/"
         item['title'] = item['titleCN']
@@ -27,8 +21,8 @@ for item in data:
         item['title'] = item['titleUS']
         item['subtitle'] = item['subtitleUS']
         result.append(item)
-    else:
-        parts = item['title'].split(' ')
+    elif os.environ['alfred_workflow_keyword'].lower() == "sxl":
+        parts = item['titleCN'].split(' ')
         text = ''.join(parts[1:]).replace(' ', '')
         formatted_title = '{:0>4}.{}.html'.format(parts[0], text)
         item["arg"] = f"https://programmercarl.com/{formatted_title}"
